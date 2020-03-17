1b34303c106df640cc8b80987e51f94e8629ee79
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-17 17:39:53 +0100

    Initial commit

---
+++ src/test/java/pl/kukla/krzys/in28minutes/microservice/currencyexchangeservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.in28minutes.microservice.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
